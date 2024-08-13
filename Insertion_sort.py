0373ce3a3fbe0b092dad642b8a5cd35b50d9c757
Akansha Daiya <71609309+Akansha-Daiya@users.noreply.github.com>
2024-08-13 13:28:24 +0530

    Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,30 +1,29 @@
-# Optimized Python program for implementation of Bubble Sort
+# Python program for implementation of Insertion Sort
 
-def bubbleSort(arr):
-    n = len(arr)    
-    # Traverse through all array elements
-    for i in range(n):
-        swapped = False
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
-            # Traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-        if (swapped == False):
-            break
+# Function to sort array using insertion sort
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
 
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
-# Driver code to test above
-if __name__ == "__main__":
-    arr = [64, 34, 25, 12, 22, 11, 90]
-
-    bubbleSort(arr)
-
-    print("Sorted array:")
+# A utility function to print array of size n
+def printArray(arr):
     for i in range(len(arr)):
-        print("%d" % arr[i], end=" ")
+        print(arr[i], end=" ")
+    print()
 
-# This code is modified by Suraj krushna Yadav
+# Driver method
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6]
+    insertionSort(arr)
+    printArray(arr)
+
+
